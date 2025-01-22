96dda292e0ef7e247f185f1a8d19851b398bb88d
Robin Moretti <robinhenrimoretti@gmail.com>
2025-01-22 14:56:05 +0100

    start to integrate LMStudio SDK

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import { LMStudioClient } from "@lmstudio/sdk";
 
-setupCounter(document.querySelector('#counter'))
+const client = new LMStudioClient();
+
+// Load a model
+const llama3 = await client.llm.load("lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF");
+
+// Create a text completion prediction
+const prediction = llama3.complete("The meaning of life is");
+
+// Stream the response
+for await (const { content } of prediction) {
+    process.stdout.write(content);
+}
